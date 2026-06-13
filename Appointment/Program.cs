6d6a12100704739407feea5366c3374b828f72fa
Robert Sendys <s29640@pjwstk.edu.pl>
2026-06-13 19:45:05 +0200

    Dodaj Swagger

--- Appointment/Program.cs
+++ Appointment/Program.cs
@@ -4,12 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
 
 app.Run();
+
+
